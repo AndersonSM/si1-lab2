24e390b4d2b26a9de659991b89b4b2496e6d9505
AndersonSM <anderson.menezes@ccc.ufcg.edu.br>
2014-12-06 02:05:25 -0300

    Alteração em testes

--- metasSemanais/test/IntegrationTest.java
+++ metasSemanais/test/IntegrationTest.java
@@ -1,12 +1,16 @@
-import org.junit.*;
+import java.util.logging.Level;
 
+import org.junit.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+import play.Logger;
 import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
-
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
@@ -17,10 +21,11 @@ public class IntegrationTest {
      */
     @Test
     public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+    	HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), driver, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                assertThat(browser.pageSource()).contains("Metas:");
             }
         });
     }
